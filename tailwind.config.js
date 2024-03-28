5c6c165a5b546a5fab7ed6deaec643a57ab66afc
mauLovera <mauricioloveraedu@gmail.com>
2024-03-28 08:41:13 -0400

    feat: tailwind setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    colors: {
+      yellow: "hsl(47, 88%, 63%)",
+      white: "hsl(0, 0%, 100%)",
+      grey: "hsl(0, 0%, 50%)",
+      black: "hsl(0, 0%, 7%)",
+    }
   },
   plugins: [],
 }
