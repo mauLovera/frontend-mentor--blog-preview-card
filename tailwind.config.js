8378ca210bfec8bddce44d2022db0e41f02fa1e5
mauLovera <mauricioloveraedu@gmail.com>
2024-04-30 17:27:08 -0400

    update: styles && tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,17 +3,77 @@ module.exports = {
   content: ["index.html"],
   theme: {
     colors: {
-      yellow: "hsl(47, 88%, 63%)",
+      "primary-yellow": "hsl(47, 88%, 63%)",
       white: "hsl(0, 0%, 100%)",
       grey: "hsl(0, 0%, 50%)",
       black: "hsl(0, 0%, 7%)",
     },
+    maxWidth: {
+      card: "384px",
+    },
     dropShadow: {
-      black: "8px 8px hsl(0, 0%, 0%)",
-      "black-hover": "16px 16px hsl(0, 0%, 0%)",
+      card: "8px 8px hsl(0, 0%, 7%)",
+      "card-hover": "16px 16px hsl(0, 0%, 7%)",
     },
     screens: {
       sm: "425px",
+    },
+    fontSize: {
+      xl: [
+        "24px",
+        {
+          lineHeight: 1.5,
+          fontWeight: 800,
+        },
+      ],
+      lg: [
+        "20px",
+        {
+          lineHeight: 1.5,
+          fontWeight: 800,
+        },
+      ],
+      body: [
+        "16px",
+        {
+          lineHeight: 1.5,
+          fontWeight: 500,
+        },
+      ],
+      "body-sm": [
+        "14px",
+        {
+          lineHeight: 1.5,
+          fontWeight: 500,
+        },
+      ],
+      "blog-author": [
+        "14px",
+        {
+          lineHeight: 1.5,
+          fontWeight: 800,
+        },
+      ],
+      "blog-genre": [
+        "14px",
+        {
+          lineHeight: 1.5,
+          fontWeight: 800,
+        },
+      ],
+      "blog-genre-sm": [
+        "12px",
+        {
+          lineHeight: 1.5,
+          fontWeight: 800,
+        },
+      ],
+    },
+    extend: {
+      borderRadius: {
+        card: "20px",
+        "card-image": "10px",
+      },
     },
   },
   plugins: [],
