976619bb7379b600e21a29aefb922996f530d6f9
mauLovera <mauricioloveraedu@gmail.com>
2024-04-30 16:37:45 -0400

    update: styles && add hover state

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["index.html"],
-	theme: {
-		colors: {
-			yellow: "hsl(47, 88%, 63%)",
-			white: "hsl(0, 0%, 100%)",
-			grey: "hsl(0, 0%, 50%)",
-			black: "hsl(0, 0%, 7%)",
-		},
-		dropShadow: {
-			black: "8px 8px hsl(0, 0%, 0%)",
-		},
-	},
-	plugins: [],
-}
+  content: ["index.html"],
+  theme: {
+    colors: {
+      yellow: "hsl(47, 88%, 63%)",
+      white: "hsl(0, 0%, 100%)",
+      grey: "hsl(0, 0%, 50%)",
+      black: "hsl(0, 0%, 7%)",
+    },
+    dropShadow: {
+      black: "8px 8px hsl(0, 0%, 0%)",
+      "black-hover": "16px 16px hsl(0, 0%, 0%)",
+    },
+    screens: {
+      sm: "425px",
+    },
+  },
+  plugins: [],
+};
